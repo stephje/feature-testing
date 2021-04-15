a078a1463d435ad291115c2d53b8537c3337f801
stephje <s.jenkins3018@gmail.com>
2021-04-15 11:23:13 +0000

    Add button to get geolocation

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//select the button
+var testButton = document.querySelector("#test-button")
+
+//add an event listener to the button that calls getLocation
+testButton.addEventListener("click", getLocation)
+
+//
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(success, error, options)
+    // console.log("this is the position", position)
+}
+
+//get the user's current location
+function success(pos){
+    var crd = pos.coords;
+    var lat = crd.latitude;
+    var lon=crd.longitude;
+    console.log("this is the coordinates", crd);
+    console.log("this is the latitude:", lat);
+    console.log("this is the longitude:", lon);
+}
+
+const options = {
+    enableHighAccuracy: true,
+  };
+
+function error(){
+    console.log("this did not work")
+}
